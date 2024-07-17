be7d10f4de177d8862ad8f57b630b4dd10bf55fb
KauanIzidoro <cnttbussineskauan@gmail.com>
2024-07-17 14:34:00 -0300

    code: solve 1083 CSES problem

--- 1083.cpp
+++ 1083.cpp
@@ -1,7 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// Solution 1: 
 int main(){
-
+    long n;
+    cin >> n;
+    long nums[n-1];
+    long arr[n-1];
+    for(int i=0;i<n;i++){
+        cin >> nums[i];
+    }
+    for(int i=0;i<n;i++){arr[i] = i+1;}
+    sort(nums,nums+n); //std::sort O(nlogn)
+    for(int k=0;k<n;k++){
+        if(nums[k] != arr[k]){
+            cout << arr[k] << endl;
+            return 0;
+        }
+    }
+    
     return 0;
 }
+
